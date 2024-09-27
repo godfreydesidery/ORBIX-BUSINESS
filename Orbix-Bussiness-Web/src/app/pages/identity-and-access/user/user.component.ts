d291b74858d3c6660f35bc30c58703b8895fd93c
Godfrey <desideryg@gmail.com>
2024-09-27 17:05:26 +0300

    Regular update

--- Orbix-Bussiness-Web/src/app/pages/identity-and-access/user/user.component.ts
+++ Orbix-Bussiness-Web/src/app/pages/identity-and-access/user/user.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { DatatableComponent, NgxDatatableModule, SelectionType } from '@swimlane/ngx-datatable';
+import { DirectivesModule } from 'src/app/theme/directives/directives.module';
 
 @Component({
   selector: 'az-user',
   standalone: true,
-  imports: [],
+  imports: [
+    DirectivesModule,
+    NgxDatatableModule
+  ],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class UserComponent {
-
+  
 }
